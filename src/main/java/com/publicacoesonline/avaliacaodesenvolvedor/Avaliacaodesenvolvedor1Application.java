92abe94a0a285f76c6c86544d0067daf2db4d9f5
IvanSJr <ivan.junior2706@gmail.com>
2021-08-07 10:17:29 -0300

    Iniciando projeto Spring Boot com Postgres.

---
+++ src/main/java/com/publicacoesonline/avaliacaodesenvolvedor/Avaliacaodesenvolvedor1Application.java
@@ -0,0 +1,13 @@
+package com.publicacoesonline.avaliacaodesenvolvedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avaliacaodesenvolvedor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avaliacaodesenvolvedor1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicacoesonline/avaliacaodesenvolvedor/Avaliacaodesenvolvedor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicacoesonline.avaliacaodesenvolvedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avaliacaodesenvolvedor1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
